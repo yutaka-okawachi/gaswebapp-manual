8f9e185baae8f1d20f94539642e802b75eed0f66
Yutaka Okawachi <pistares@gmail.com>
2025-12-27 21:25:36 +0900

    feat: 用語集ページに注記を追加
    RSの管弦楽曲、Bruckner、Hindemith、Karg-Elertの2語以上の用語は
    最初の単語で検索する旨の説明を追加

--- src/generate_dic_html.js
+++ src/generate_dic_html.js
@@ -517,6 +517,9 @@ function generateDicHtml(dicData, abbrData) {
         <div class="container">
             <div class="home-link"><a href="home.html">HOME</a></div>
             <h1>用語集</h1>
+            <p style="font-size: 0.85rem; color: #555; margin-top: 0.5em; margin-bottom: 1em;">
+                RSの管弦楽曲，Anton Bruckner，Paul Hindemith, Sigfrid Karg-Elert については楽譜に記されている2語以上のものについては最初の単語で検索
+            </p>
             <div id="listContainer">
 ${dicListHtml}
             </div>
